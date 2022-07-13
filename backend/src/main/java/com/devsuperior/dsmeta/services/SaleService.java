c76a2b5d608ac425da0d53e0cb6fcbe94e89755e
giosantoro <giggio.santoro@gmail.com>
2022-07-13 10:55:33 -0300

    API test

---
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dsmeta.services;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SaleService {
+    private final SaleRepository repository;
+
+    public SaleService(SaleRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Sale> findSales() {
+        return repository.findAll();
+    }
+}
